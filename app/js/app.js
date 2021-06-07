1b0d731a76bc31d5ae61e0d3bdc62c5f82990293
kolesnikov <shadarsi@gmail.com>
2021-06-07 18:01:25 +0300

    add some sections

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,20 @@
 // // Import jQuery module (npm i jquery)
-import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+//import $ from 'jquery'
+//window.jQuery = $
+//window.$ = $
 
 // // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+require('~/app/libs/lightzoom/lightzoom.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
 
 })
+$( document ).ready(function() {
+    $('.lightzoom').lightzoom(
+        {
+            isOverlayClickClosing: true
+        }
+    );
+});
