b7fd2a3dbb3e547839b1dbe4f080dd0a5dacd384
aldaces <shadarsi@gmail.com>
2021-06-08 14:47:41 +0300

    desctop end

--- app/js/app.js
+++ app/js/app.js
@@ -8,13 +8,22 @@ require('~/app/libs/lightzoom/lightzoom.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
-
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 })
-$( document ).ready(function() {
-    $('.lightzoom').lightzoom(
-        {
-            isOverlayClickClosing: true
-        }
-    );
+$(document).ready(function () {
+    $('.lightzoom').lightzoom({
+        isOverlayClickClosing: true
+    });
 });
